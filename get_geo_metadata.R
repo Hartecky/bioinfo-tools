c2f6383f6e045daadc378d11b0c83a4f169ea650
Hartecky <hofman.bartlomiej@gmail.com>
2025-03-31 09:02:02 +0200

    corrected writing table

--- get_geo_metadata.R
+++ get_geo_metadata.R
@@ -1,7 +1,7 @@
 library(GEOquery)
 library(tidyverse)
 
-get_geo_metadata <- function(geo_id) {
+get_geo_metadata <- function(geo_id, file = FALSE) {
   metadata <- getGEO(GEO = geo_id, GSEMatrix = FALSE)
   metadata <- purrr::map(metadata@gsms, ~.x@header$characteristics_ch1) %>%
   stack() %>%
@@ -9,14 +9,16 @@ get_geo_metadata <- function(geo_id) {
   pivot_wider(names_from= feature, values_from = value) %>%
   janitor::clean_names()
  
-  write.table(metadata, 
-              file = paste(geo_id, "metadata.txt", sep = "_"),
-              sep = "\t",
-              col.names = TRUE,
-              row.names = FALSE,
-              quote = FALSE)
-  
+  if (isTRUE(file)) {
+    write.table(metadata,
+                file = paste(geo_id,"metadata.txt", sep = "_"),
+                sep = "\t",
+                col.names = TRUE,
+                row.names = FALSE,
+                quote = FALSE)
+    
+  }
+
   return(metadata) 
 }
 
-meta <- get_geo_metadata("GSE60052")
